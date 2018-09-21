fa697718083d28c13b902ef67911b0bfc61c827b
Samuel Pozaicer <sam.pz3r@gmail.com>
2018-09-21 13:44:05 -0500

    gen folder new structure

--- tags.js
+++ tags.js
@@ -14,7 +14,7 @@
     var temp = []
     for (i = 0, j = data.length, k = 1; i < j; i += chunkSize, k++) {
       temp = data.slice(i, i + chunkSize)
-      let result = await generateHtml('./templates/tags.ejs', temp, `./gen/tags/tags_${today.getTime()}_${k}.html`)
+      let result = await generateHtml('./templates/tags.ejs', temp, `./gen/tags/html/tags_${today.getTime()}_${k}.html`)
       finalResult += result
     }
 
